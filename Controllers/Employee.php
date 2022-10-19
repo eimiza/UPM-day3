7a416cebb311012a2b6526a09c724c7213ba274c
Eimiza Faisha Azmi <eimiza@ict.upsi.edu.my>
2022-10-19 15:44:25 +0800

    Update Employee.php

--- Controllers/Employee.php
+++ Controllers/Employee.php
@@ -7,15 +7,23 @@ use App\Libraries\Secure;
 
 class Employee extends BaseController
 {
+    public function __construct(){
+        $this->mod = model('App\Models\EmployeeModel');
+    }
+
     public function index()
     {
         $data['title'] = 'Employee';
         $this->render('employee', $data);
+       
     }
 
     public function download_pdf(){
+        $data['data'] = $this->mod->findAll();
+        $html = view('emp_pdf',$data);
+        //return $html;
         $mpdf = new \Mpdf\Mpdf();
-        $mpdf->WriteHTML('<h1>Hello world!</h1>');
+        $mpdf->WriteHTML($html);
         $mpdf->Output();
         
         header("Content-type:application/pdf");
